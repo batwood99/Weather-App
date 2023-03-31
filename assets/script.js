b21a6eb10f06e2119d792291019801c76a1cc75a
batwood99 <atwood@mail.usf.edu>
2023-03-31 13:43:01 -0400

    added initial HTML, Js, and css files and added pseudocode

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+// //write code to retrieve the city name from the form input when the user submits the form.
+// Use the Open Weather Map API to retrieve the current weather data for the city entered by the user.
+// Display the current weather information on the webpage, including the city name, date, weather icon, temperature, humidity, and wind speed.
+// Use the Open Weather Map API to retrieve the 5-day forecast data for the city entered by the user.
+// Display the 5-day forecast information on the webpage, including the date, weather icon, temperature, wind speed, and humidity.
+// Implement a search history feature by storing the searched city names in the browser's local storage.
+// Create a function to retrieve the search history from local storage and display it on the webpage.
+// Add an event listener to the search history list items, so that when a user clicks on a previously searched city, the current and future weather conditions for that city are displayed.
